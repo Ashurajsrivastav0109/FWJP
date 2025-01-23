7c0cac63f87875cdf76828b5410c0e6490e521e9
Ashurajsrivastav0109 <ashuraj1357@gmail.com>
2025-01-23 15:28:14 +0530

    Add files via upload

---
+++ Lab - 01 Core Java/Day 03/q4.java
@@ -0,0 +1,8 @@
+class q4 {
+    public static void main(String args[]) {
+        double pi = 3.14;
+        double radius = 10;
+        double area = pi * radius * radius;
+        System.out.println("The area of the circle is: " + area);
+    }
+}
